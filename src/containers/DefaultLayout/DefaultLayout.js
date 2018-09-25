91461828d01d9a332243142287892e37ebd5c017
Nnoitra Gilga <35554978+NNOITl2A@users.noreply.github.com>
2018-09-25 13:17:08 +0700

    set routes

---
+++ src/containers/DefaultLayout/DefaultLayout.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import { Container } from 'reactstrap';
+
+// import {
+//   AppAside,
+//   AppBreadcrumb,
+//   AppFooter,
+//   AppHeader,
+//   AppSidebar,
+//   AppSidebarFooter,
+//   AppSidebarForm,
+//   AppSidebarHeader,
+//   AppSidebarMinimizer,
+//   AppSidebarNav,
+// } from '@coreui/react';
+
+// sidebar nav config
+// import navigation from '../../_nav';
+// routes config
+// import routes from '../../routes';
+// import DefaultAside from './DefaultAside';
+// import DefaultFooter from './DefaultFooter';
+// import DefaultHeader from './DefaultHeader';
+
+class DefaultLayout extends Component {
+  render() {
+    return (
+      <div className="web">
+      
+        Hello World 555 
+      </div>
+    );
+  }
+}
+
+export default DefaultLayout;
